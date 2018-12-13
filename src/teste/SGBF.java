19ef678671ec9bcd2e34ef6345c939c37fb9ff56
Marron <Marron@General-Marron>
2018-12-13 21:15:52 +0100

    UGS

--- src/teste/SGBF.java
+++ src/teste/SGBF.java
@@ -22,7 +22,7 @@ public class SGBF extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World  - Djavam Amaro'");
+        btn.setText("Say 'Hello World  - Djavam king'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
