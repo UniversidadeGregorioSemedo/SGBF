ac1f81849bcd202c1e9c0965a529ac99d54ca46b
Look <Look@DESKTOP-DNL1D53>
2018-12-13 19:54:00 +0000

    Djavam

--- src/teste/SGBF.java
+++ src/teste/SGBF.java
@@ -22,7 +22,7 @@ public class SGBF extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello Worldh'");
+        btn.setText("Say 'Hello Worldh - Djavam'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
