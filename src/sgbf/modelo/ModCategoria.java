2a04e79013d3291f6d7f3edb145cb4d2e667ec33
Look <Look@DESKTOP-DNL1D53>
2018-12-17 15:59:18 +0000

    Tratamento das Categorias

--- src/sgbf/modelo/ModCategoria.java
+++ src/sgbf/modelo/ModCategoria.java
@@ -6,6 +6,8 @@
 package sgbf.modelo;
 
 import sgbf.util.UtilControloDaData;
+import sgbf.util.UtilControloExcessao;
+import sgbf.util.UtilIconesDaJOPtionPane;
 
 /**
  *
@@ -13,11 +15,13 @@ import sgbf.util.UtilControloDaData;
  */
 public class ModCategoria {
     private Integer idCategoria;
+    private String nome;
     private String data_registo;
     private String data_modificacao;
     
     public ModCategoria(){
         this.idCategoria = 0;
+        this.nome = null;
         this.data_registo = String.valueOf(UtilControloDaData.dataActual());
         this.data_modificacao = String.valueOf(UtilControloDaData.dataActual());
     }
@@ -26,15 +30,31 @@ public class ModCategoria {
         return idCategoria;
     }
 
-    public void setIdCategoria(Integer idCategoria, String opercao) {
+    public void setIdCategoria(Integer idCategoria, String operacao) {
         this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome, String operacao) {
+        if(nome == null){
+            throw new UtilControloExcessao("Categoria não definida !", operacao, UtilIconesDaJOPtionPane.Advertencia.nomeDaImagem());
+        }else{
+            if(nome.isEmpty()){
+                throw new UtilControloExcessao("Categoria não definida !", operacao, UtilIconesDaJOPtionPane.Advertencia.nomeDaImagem());
+            }else{
+                this.nome = nome;
+            }
+        }
     }
 
     public String getData_registo() {
         return data_registo;
     }
 
-    public void setData_registo(String data_registo, String opercao) {
+    public void setData_registo(String data_registo, String operacao) {
         this.data_registo = data_registo;
     }
 
@@ -45,4 +65,11 @@ public class ModCategoria {
     public void setData_modificacao(String data_modificacao, String opercao) {
         this.data_modificacao = data_modificacao;
     }
+    
+    public void equals(ModCategoria categoriaMod, String operacao){
+        if(this.nome.equalsIgnoreCase(categoriaMod.nome)){
+            throw new UtilControloExcessao("Já existe uma Categoria com este Nome !", operacao, UtilIconesDaJOPtionPane.Advertencia.nomeDaImagem());
+        }
+    }
+    
 }
