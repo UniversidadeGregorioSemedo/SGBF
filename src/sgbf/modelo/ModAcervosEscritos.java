c98ba4aa85c10924950646b9ff77e51eef24d999
Look <Look@DESKTOP-DNL1D53>
2018-12-17 13:30:18 +0000

    Responsável por relacionar os autores com os acervos escritos

---
+++ src/sgbf/modelo/ModAcervosEscritos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgbf.modelo;
+
+/**
+ *
+ * @author Look
+ */
+public class ModAcervosEscritos {
+    private ModAcervo acervoMod;
+    private ModAutor autorMod;
+
+    public ModAcervo getAcervoMod() {
+        return acervoMod;
+    }
+
+    public void setAcervoMod(ModAcervo acervoMod) {
+        this.acervoMod = acervoMod;
+    }
+
+    public ModAutor getAutorMod() {
+        return autorMod;
+    }
+
+    public void setAutorMod(ModAutor autorMod) {
+        this.autorMod = autorMod;
+    }
+    
+}
