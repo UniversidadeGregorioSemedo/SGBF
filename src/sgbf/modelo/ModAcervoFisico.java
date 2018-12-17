ac457bcf47167623e3ae7b168fca0317156a44ab
Look <Look@DESKTOP-DNL1D53>
2018-12-17 15:59:03 +0000

    Tratamento dos Acervos Físicos

--- src/sgbf/modelo/ModAcervoFisico.java
+++ src/sgbf/modelo/ModAcervoFisico.java
@@ -5,10 +5,50 @@
  */
 package sgbf.modelo;
 
+import sgbf.util.UtilControloDaData;
+
 /**
  *
  * @author Look
  */
-public final class ModAcervoFisico {
+public final class ModAcervoFisico extends ModAcervo{
+    
+    private String codigo_barra;
+    private String isbn;
+    
+    public ModAcervoFisico(){
+        this.idAcervo = 0;
+        this.titulo = null;
+        this.sub_titulo = null;
+        this.tipo_acervo = null;
+        this.formato = null;
+        this.edicao = 0;
+        this.volume = 0;
+        this.numero_paginas = 0;
+        this.ano_lancamento = 0;
+        this.idioma = null;
+        this.codigo_barra = null;
+        this.isbn = null;
+        this.estoqueMod = new ModEstoque();
+        this.categoriaMod = new ModCategoria();
+        this.data_registo = String.valueOf(UtilControloDaData.dataActual());
+        this.data_modificacao = String.valueOf(UtilControloDaData.dataActual());
+    }
+
+    public String getCodigo_barra() {
+        return codigo_barra;
+    }
+
+    public void setCodigo_barra(String codigo_barra, String operacao) {
+        this.codigo_barra = codigo_barra;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn, String operacao) {
+        this.isbn = isbn;
+    }
     
 }
