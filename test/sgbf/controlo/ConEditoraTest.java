46723098d0787f2762d807500c21fa6462486c53
Look <Look@DESKTOP-DNL1D53>
2018-12-17 11:32:33 +0000

    Classe de Teste com JUnit para ModEditora

---
+++ test/sgbf/controlo/ConEditoraTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgbf.controlo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Look
+ */
+public class ConEditoraTest {
+    
+    public ConEditoraTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testRegistar() {
+    }
+
+    @Test
+    public void testAlterar() {
+    }
+
+    @Test
+    public void testRemover() {
+    }
+
+    @Test
+    public void testListarTodos() {
+    }
+
+    @Test
+    public void testPesquisar() {
+    }
+    
+}
