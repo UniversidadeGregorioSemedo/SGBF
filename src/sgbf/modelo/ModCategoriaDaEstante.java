65e9459882191e43c75962ae48a9fede32bce573
Look <Look@DESKTOP-DNL1D53>
2018-12-17 14:40:54 +0000

    Responsável pelpor relacionar a categoria com a Estante

---
+++ src/sgbf/modelo/ModCategoriaDaEstante.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgbf.modelo;
+
+/**
+ *
+ * @author Look
+ */
+public class ModCategoriaDaEstante {
+    
+    private ModCategoria categoriaMod;
+    private ModEstante estanteMod;
+
+    public ModCategoria getCategoriaMod() {
+        return categoriaMod;
+    }
+
+    public void setCategoriaMod(ModCategoria categoriaMod) {
+        this.categoriaMod = categoriaMod;
+    }
+
+    public ModEstante getEstanteMod() {
+        return estanteMod;
+    }
+
+    public void setEstanteMod(ModEstante estanteMod) {
+        this.estanteMod = estanteMod;
+    }
+    
+}
