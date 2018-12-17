dd5f9158c120f98139759e98457441f9c70bc8dc
Look <Look@DESKTOP-DNL1D53>
2018-12-17 10:33:22 +0000

    Testando o comportamento da datas da aplicação para a base de dados

---
+++ src/sgbf/controlo/ConEditora.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgbf.controlo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Look
+ */
+public class ConEditora extends ConCRUD{
+
+    @Override
+    public boolean registar(Object objecto_registar, String operacao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean alterar(Object objecto_alterar, String operacao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean remover(Object objecto_remover, String operacao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Object> listarTodos(String operacao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Object> pesquisar(Object objecto_pesquisar, String operacao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
