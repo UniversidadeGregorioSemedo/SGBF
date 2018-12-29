e205702d9d752abcda5085c12a3b6244e4a79022
Marron <Marron@General-Marron>
2018-12-29 20:54:59 +0100

    Controlador menu

---
+++ src/sgbf/controlo/VisTelaPrincipalAdmController.java
@@ -0,0 +1,73 @@
+package sgbf.controlo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author Marron
+ */
+public class VisTelaPrincipalAdmController implements Initializable {
+
+     @FXML
+    private MenuItem menuitemCadastroAcervo;
+    @FXML
+    private MenuItem menuitemCadastroArea;
+    @FXML
+    private MenuItem menuitemCadastroAutor;
+    @FXML
+    private MenuItem menuitemCadastroCategoria;
+    @FXML
+    private MenuItem menuitemCadastroEditora;
+    @FXML
+    private MenuItem menuitemCadastroEstante;
+    @FXML
+    private MenuItem menuitemCadastroEstoque;
+    @FXML
+    private MenuItem menuitemCadastroFuncionario;
+    @FXML
+    private MenuItem menuitemCadastroProveniencia;
+    @FXML
+    private MenuItem menuitemCadastroUtente;
+    @FXML
+    private MenuItem menuitemVerUtentes;
+    @FXML
+    private MenuItem menuitemVerProveniencias;
+    @FXML
+    private MenuItem menuitemVerFuncionarios;
+    @FXML
+    private MenuItem menuitemVerEstoques;
+    @FXML
+    private MenuItem menuitemVerEstantes;
+    @FXML
+    private MenuItem menuitemVerEditoras;
+    @FXML
+    private MenuItem menuitemVerCategorias;
+    @FXML
+    private MenuItem menuitemVerAutores;
+    @FXML
+    private MenuItem menuitemVerAreas;
+    @FXML
+    private MenuItem menuitenmVerAcervos;
+    @FXML
+    private MenuItem menuitemMovimentacaoEmprestimo;
+    @FXML
+    private MenuItem menuitemMovimentacaoReserva;
+    @FXML
+    private MenuItem menuitemMovimentacaoDevolucao;
+    @FXML
+    private AnchorPane anchorPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    
+}
