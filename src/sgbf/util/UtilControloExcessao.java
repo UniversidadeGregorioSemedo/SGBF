7ec8436c8749068bdff9b97992ac5d7ff53ce270
Francisco Lourenço <franciscolour2011@gmail.com>
2018-12-30 19:14:36 +0000

    Dialog do Javafx

--- src/sgbf/util/UtilControloExcessao.java
+++ src/sgbf/util/UtilControloExcessao.java
@@ -2,6 +2,7 @@
 
 package sgbf.util;
 
+import javafx.scene.control.Alert;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -12,11 +13,18 @@ import javax.swing.JOptionPane;
 
 public class UtilControloExcessao extends RuntimeException {
 
-   
-    
     public UtilControloExcessao(String messagem, String tituloDaJanela, String icone){
         super(messagem);
         ImageIcon imagem = new ImageIcon(icone);
         JOptionPane.showMessageDialog(null,super.getMessage(), tituloDaJanela, JOptionPane.INFORMATION_MESSAGE,imagem);
+    }
+    
+    public UtilControloExcessao(String titulo, String mensagem, Alert.AlertType icone){
+        super(mensagem);
+        Alert alert = new Alert(icone);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensagem);
+        alert.showAndWait();
     }
 }
