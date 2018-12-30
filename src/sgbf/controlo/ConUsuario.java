f79a05b5561dbc26683959ccc6e2dcb9817f60c2
Francisco Lourenço <franciscolour2011@gmail.com>
2018-12-30 09:03:20 +0000

    Método de autenticação

--- src/sgbf/controlo/ConUsuario.java
+++ src/sgbf/controlo/ConUsuario.java
@@ -5,6 +5,12 @@
  */
 package sgbf.controlo;
 
+import java.sql.SQLException;
+import sgbf.modelo.ModFuncionario;
+import sgbf.modelo.ModUtente;
+import sgbf.util.UtilControloExcessao;
+import sgbf.util.UtilIconesDaJOPtionPane;
+
 
 /**
  *
@@ -12,5 +18,23 @@ package sgbf.controlo;
  */
 public class ConUsuario {
     
+    private ConUtente utenteCon;
+    
+    public ConUsuario(){
+        this.utenteCon = new ConUtente();
+    }
+    
+    public boolean autenticar(ModUtente utenteMod, String operacao){
+        try{
+            utenteCon.query = "select * from usuario where usuario=? and senha=?";
+            utenteCon.preparedStatement = utenteCon.caminhoDaBaseDados.baseDeDados(operacao).prepareStatement(utenteCon.query);
+            utenteCon.preparedStatement.setString(1, utenteMod.getUsuario());
+            utenteCon.preparedStatement.setString(2, utenteMod.getSenha());
+            utenteCon.setResultset = utenteCon.preparedStatement.executeQuery();
+            return !utenteCon.setResultset.next();
+        }catch(SQLException erro){
+            throw new UtilControloExcessao("Erro ao iniciar sessão\nErro: "+erro.getMessage(), operacao, UtilIconesDaJOPtionPane.Erro.nomeDaImagem());
+        }
+    }
     
 }
