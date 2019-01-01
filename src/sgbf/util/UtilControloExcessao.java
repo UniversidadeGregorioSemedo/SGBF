dd1b3a65bf1f713c4ef1fac0cea22b6afc9d6bce
Francisco Lourenço <franciscolour2011@gmail.com>
2019-01-01 23:38:15 +0000

    Caixa de diálogo de confirmação

--- src/sgbf/util/UtilControloExcessao.java
+++ src/sgbf/util/UtilControloExcessao.java
@@ -2,7 +2,9 @@
 
 package sgbf.util;
 
+import java.util.Optional;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -13,11 +15,15 @@ import javax.swing.JOptionPane;
 
 public class UtilControloExcessao extends RuntimeException {
 
+    public UtilControloExcessao(){
+    }
+    
     public UtilControloExcessao(String messagem, String tituloDaJanela, String icone){
         super(messagem);
         ImageIcon imagem = new ImageIcon(icone);
         JOptionPane.showMessageDialog(null,super.getMessage(), tituloDaJanela, JOptionPane.INFORMATION_MESSAGE,imagem);
     }
+    
     
     public UtilControloExcessao(String titulo, String mensagem, Alert.AlertType icone){
         super(mensagem);
@@ -27,4 +33,20 @@ public class UtilControloExcessao extends RuntimeException {
         alert.setContentText(mensagem);
         alert.showAndWait();
     }
+    
+    public boolean temCerteza(String operacao, String mensagem){
+        Alert confirmacao = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmacao.setTitle(operacao);
+        confirmacao.setHeaderText(null);
+        confirmacao.setContentText(mensagem);
+         
+        ButtonType SIM = new ButtonType("Sim");
+        ButtonType CANCELAR = new ButtonType("Cancelar");
+        
+        confirmacao.getButtonTypes().setAll(SIM, CANCELAR);
+        
+        Optional<ButtonType> opccao = confirmacao.showAndWait();
+        return opccao.get() == SIM;
+    }
+   
 }
