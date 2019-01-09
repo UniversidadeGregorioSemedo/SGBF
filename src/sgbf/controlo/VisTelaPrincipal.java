6314b2e72e48c1bb93de4a33d47a4a66548a3e03
Marron <Marron@General-Marron>
2019-01-09 12:42:15 +0100

    metodo para ver chamar tela ver acervo

--- src/sgbf/controlo/VisTelaPrincipal.java
+++ src/sgbf/controlo/VisTelaPrincipal.java
@@ -189,6 +189,11 @@ public class VisTelaPrincipal implements Initializable {
         AnchorPane x = (AnchorPane) FXMLLoader.load(getClass().getResource("/sgbf/visao/TabelaVerUtente.fxml"));
         anchorPane.getChildren().setAll(x);         
     }
+    @FXML
+    public void BotaoMenuItemVerAcervo() throws IOException{
+        AnchorPane x = (AnchorPane) FXMLLoader.load(getClass().getResource("/sgbf/visao/TabelaVerAcervo.fxml"));
+        anchorPane.getChildren().setAll(x);         
+    }
     
     @FXML
     public void botaoMenuItemTerminarSessao()  throws IOException{
