1dfdd1ae571285762e6b0f5f31d5459e5de1586e
Francisco Lourenço <franciscolour2011@gmail.com>
2019-01-12 22:40:38 +0000

    Interface de cadastramento refactorado

--- src/sgbf/controlo/VisCadastramentoAcervo.java
+++ src/sgbf/controlo/VisCadastramentoAcervo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sgbf.controlo;
 
 import java.net.URL;
@@ -16,9 +11,7 @@ import javafx.fxml.Initializable;
  */
 public class VisCadastramentoAcervo implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
